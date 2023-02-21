f9cf728621371c227b44cbe7a1dc38485ed10fc1
ErnestH1 <hansonernest7@gmail.com>
2023-02-21 11:02:23 +0300

    completed

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
-end
+def countdown(n)
+
+    while n > 0
+      puts "#{n} SECOND(S)!"
+      n -= 1
+    end
+    
+    "HAPPY NEW YEAR!"
+  end
+  
+  def countdown_with_sleep(n)
+    sleep 5
+
+    while n > 0
+      puts "#{n} SECOND(S)!"
+      n -= 1
+    end
+    
+    "HAPPY NEW YEAR!"
+  end
+
+  countdown(5)
+  countdown_with_sleep(6)
